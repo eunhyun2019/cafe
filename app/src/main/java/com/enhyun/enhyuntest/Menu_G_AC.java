a67e4fc09ca899ec192407276745e0f093fd55bc
soohyun <68890903+gongmikim@users.noreply.github.com>
2021-06-05 17:12:44 +0900

    - 20210605 김수현 수정 및 추가 1. menu activuty 수정 2. menu java 수정

--- app/src/main/java/com/enhyun/enhyuntest/Menu_G_AC.java
+++ app/src/main/java/com/enhyun/enhyuntest/Menu_G_AC.java
@@ -2,13 +2,47 @@ package com.enhyun.enhyuntest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TabHost;
 
-public class Menu_G_AC extends AppCompatActivity {
+public class Menu_G_AC extends TabActivity {
+    Button btnCoffee, btnLatte, btnTeaLatte, btnBubble, btnSmoodie, btnAde, btnDessert;
+    ListView listMenuView, listFavView;
+
+    /*ArrayList<아마db이름> listMenu;
+    ArrayList<아마db이름> listFav;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_grazie);
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpecSong = tabHost.newTabSpec("menu").setIndicator("메뉴");
+        tabSpecSong.setContent(R.id.tab_menu);
+        tabHost.addTab(tabSpecSong);
+
+        TabHost.TabSpec tabSpecArtist = tabHost.newTabSpec("fav").setIndicator("즐겨찾기");
+        tabSpecArtist.setContent(R.id.tab_fav);
+        tabHost.addTab(tabSpecArtist);
+
+        tabHost.setCurrentTab(0);
+
+        btnCoffee = findViewById(R.id.btn_coffe);
+        btnLatte = findViewById(R.id.btn_latte);
+        btnTeaLatte = findViewById(R.id.btn_teaLatte);
+        btnBubble = findViewById(R.id.btn_bubble);
+        btnSmoodie = findViewById(R.id.btn_smoodie);
+        btnAde = findViewById(R.id.btn_ade);
+        btnDessert = findViewById(R.id.btn_dessert);
+        listMenuView = findViewById(R.id.listMenuView);
+        listFavView = findViewById(R.id.listFavView);
+
+        /*listMenu = new ArrayList<>();
+        listFav = new ArrayList<>();*/
     }
 }
